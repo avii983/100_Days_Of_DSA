61708111f944a7ebcf95ebf98dc50098e53d7d8d
Avi Agarwal <aviiagarwal18@gmail.com>
2026-05-05 11:46:56 +0530

    Add files via upload

---
+++ Day094/Day94.c
@@ -0,0 +1,44 @@
+// Problem: Sort array of non-negative integers using counting sort.
+// Find max, build freq array, compute prefix sums, build output.
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+
+    int count[max + 1];
+    for (int i = 0; i <= max; i++)
+        count[i] = 0;
+
+    for (int i = 0; i < n; i++)
+        count[arr[i]]++;
+
+    for (int i = 1; i <= max; i++)
+        count[i] += count[i - 1];
+
+    int output[n];
+
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+
+    for (int i = 0; i < n; i++)
+        arr[i] = output[i];
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
