b714fbc01d33ed09fc5fd4c1ae95322ed3a10d8b
Avi Agarwal <aviiagarwal18@gmail.com>
2026-05-05 09:11:57 +0530

    Add files via upload

---
+++ Day063/Day63.c
@@ -0,0 +1,76 @@
+/*Problem: Perform DFS starting from a given source vertex using recursion.
+
+Input:
+- n
+- adjacency list
+- starting vertex s
+
+Output:
+- DFS traversal order*/
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Node* adj[MAX];
+int visited[MAX];
+
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void addEdge(int u, int v) {
+    struct Node* newNode = createNode(v);
+    newNode->next = adj[u];
+    adj[u] = newNode;
+
+    newNode = createNode(u);
+    newNode->next = adj[v];
+    adj[v] = newNode;
+}
+
+void dfs(int v) {
+    visited[v] = 1;
+    printf("%d ", v);
+
+    struct Node* temp = adj[v];
+    while (temp != NULL) {
+        int neighbor = temp->vertex;
+
+        if (!visited[neighbor]) {
+            dfs(neighbor);
+        }
+
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, m, u, v, s;
+
+    scanf("%d %d", &n, &m);
+
+    for (int i = 0; i < n; i++) {
+        adj[i] = NULL;
+        visited[i] = 0;
+    }
+
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        addEdge(u, v);
+    }
+
+    scanf("%d", &s);
+
+    dfs(s);
+
+    return 0;
+}
